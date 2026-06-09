362a1b3596037e482eeac96f65fa996827440f7c
shalini <s.rathore43@gmail.com>
2026-06-09 11:02:52 +0530

    hkjhkjh

---
+++ dict/eg1.py
@@ -0,0 +1,30 @@
+# pincodes = {"Rajkot":345424,"Ahmedabad":282425,"Vapi":456654}
+# print(pincodes)
+# print("pincode of rahkit is ", pincodes["Rajkot"])
+# for p in pincodes:
+#     print(p)
+#     print(pincodes[p])
+# for k,v in pincodes.items():
+#     print(k,"---",v)
+
+# for v in pincodes.values():
+#     print(v)
+
+students = {"Ron":[25,35,41],"John":[35,65,45],"Maria":[65,85,75]
+            ,"Sonia":[33,52,41]}
+for k,v in students.items():
+    # print(f"marks of {k} is {v}")
+    # 35-44 3rd division
+    # 45-59 2nd division
+    # first division 
+    #congratulation  Ron ,you  pass in 1st division 
+    #sorry ,Maria you are fail
+    flagPass = True
+    msg = f"{k}  Pass"
+    for m in v:
+        if(m<35):
+            flagPass=False
+            break
+    if(not flagPass):
+        msg = f"{k}  Fail"
+    print(msg)
--- loops/eg1.py
+++ loops/eg1.py
@@ -19,6 +19,7 @@
 
 # l = list(range(5))
 # print(l)
+# print("********************")
 
 # l = list(range(2,5))
 # print(l)
@@ -30,12 +31,27 @@
 # lst = list(range(1,11))
 # for  n in lst:
 #     print(n)
-
+   
 # for m in range(1,11):
 #     # print(m)
 #     if(m%2==0):
 #         print(m)
 
-for n in range(10,0,-1):
-    print(n)
+# for n in range(10,0,-1):
+#     print(n)
     # Count how many numbers are greater than 20 in [5, 25, 15, 30, 10].
+
+# n = int(input("Enter any no "))
+# for i in range(1,11):
+#     print(f"{n} X {i} = {n*i}")
+
+n = int(input("Enter any no "))
+fact = 1 
+# for i in range(1,n+1):
+for i in range(n,0,-1):
+    fact = fact * i 
+
+# 4!   = 4*3*2*1
+# 5!= 5*4*3*2*1
+# fact
+print(f" factorail of {n} is {fact}")
