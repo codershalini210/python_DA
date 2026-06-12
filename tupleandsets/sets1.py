424282da3a008ed9b1f4c64d60d1ce2b45050bc4
shalini <s.rathore43@gmail.com>
2026-06-12 10:33:05 +0530

    asdsada

---
+++ tupleandsets/sets1.py
@@ -0,0 +1,37 @@
+# set1 = {1,2,3,4,5}
+# set2 = {4,5,6,7}
+ 
+# # print(set1-set2)
+# print(set2-set1)
+# print(set2 ^ set1)
+# print(set1)
+# print(set2)
+# print(set1.union(set2))
+# print(set1.intersection(set2))
+
+# fruits = {"apple", "banana", "cherry"}
+# fruits.add("Orange")
+# fruits.update(["Kiwi","Papaya"])
+# print(fruits)
+# print(fruits.discard("litchi"))
+# # print(fruits.remove("litchi"))
+# print(fruits.pop())
+# print(fruits)
+
+# fset = frozenset([43,23,53])
+# print(fset)
+# squares = {x*x for x in range (5)}
+# print(squares)
+# for s  in squares:
+#     print(s)
+# print(45 in squares)
+# print(16 in squares)
+# print(45 not in squares)
+# print(16 not in squares)
+
+# Section 4: Sets
+# Create a set with duplicate values and print the result.
+# Add a new value to a set.
+# Remove an item from a set.
+# Find common elements between two sets.
+# Find the union of two sets.
