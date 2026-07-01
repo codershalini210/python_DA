fecc3b31b310e6d69594024da36c11134ac146c5
shalini <s.rathore43@gmail.com>
2026-07-01 11:57:40 +0530

    slkdjfl

---
+++ numpy_programs/module5.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# sales_jan = np.array([1000,2000,3000])
+# sales_feb = np.array([1500,2500,3500])
+# total_sales = sales_jan+sales_feb
+# print(total_sales)
+# print(sales_feb-sales_jan)
+# print(sales_jan*sales_feb)
+# .....................
+# salaries = np.array([25000,65222,45232,356421])
+# print(salaries)
+# print(salaries+1000)
+# print(salaries-1000)
+# print(salaries*100)
+# print(salaries/2)
+# print(salaries%2)
+# --------------
+numbers =np.array([2,4,5,3])
+print(numbers ** 2)
+print(np.power(2,5))
