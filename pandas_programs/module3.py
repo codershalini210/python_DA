c5ef54eea61e2bba45e43be1f74abeef0366181e
shalini <s.rathore43@gmail.com>
2026-07-06 12:38:05 +0530

    mgjghj

--- pandas_programs/module3.py
+++ pandas_programs/module3.py
@@ -1,3 +1,12 @@
 import pandas as pd
-df = pd.read_csv("electronics.csv")
-print(df)
+# df = pd.read_csv("electronics.csv")
+# print(df)
+df = pd.read_excel("Basic Excel.xlsx")
+# print(df.head(3))
+print("-----------------------")
+print(df[df["result"]=="Pass"])
+
+print("-----------------------")
+print(df[df["result"]!="Pass"])
+# print(df.tail())
+# pip install openpyxl
